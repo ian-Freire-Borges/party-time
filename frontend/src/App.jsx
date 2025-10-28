b529e7d8d7b0b9995f39047cd46e79c114ea1238
Ian Freire Borges <ianxofreire@gmail.com>
2025-10-28 10:23:36 -0300

    mobile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,24 +5,11 @@ import Particle from './components/particle';
 import {ToastContainer} from 'react-toastify'
 import "react-toastify/dist/ReactToastify.css"
 import Footer from './components/Footer';
-import { useState,useEffect} from "react";
+import { useState} from "react";
 
 function App() {
   const [searchParty, setSearchParty] = useState("");
-  
-    useEffect(() => {
-    const handleTouchMove = (e) => {
-      if (document.body.scrollHeight <= window.innerHeight) {
-        e.preventDefault(); // bloqueia bounce
-      }
-    };
 
-    document.addEventListener("touchmove", handleTouchMove, { passive: false });
-
-    return () => {
-      document.removeEventListener("touchmove", handleTouchMove);
-    };
-  }, []);
   
   return (
     <div>
